0ea1b0aa738503be9295ae6585769ce6beda307e
Mathis Loïc Messager <messamat@uw.edu>
2017-08-04 18:31:46 -0700

    Add header

--- app.R
+++ app.R
@@ -1,3 +1,8 @@
+#Source code for online app serving as supplemetary material to Ruhi, A., Messager, L. M., & Olden, J. D. (2017) "Consistent freshwater resource monitoring for a changing world"
+#App developer: Mathis Messager
+#Contact info: messamat@uw.edu
+#Last updated: 08/04/2017
+
 library(foreign)
 library(leaflet)
 library(leaflet.extras)
